16d0eba8d61c2f132730c89c6e94351d622d5b2b
Xiao <2501683578@qq.com>
2025-10-01 11:29:12 +0800

    fix: lint

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -67,8 +67,7 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { ProductAPI, ProductStatus } from '../services/product'
-import type { ProductInfo } from '../services/product'
+import { ProductAPI, ProductStatus, type ProductInfo } from '../services/product'
 import { notification } from '../utils/notification'
 import { handleAPIError, HTTP_STATUS } from '../services/auth'
 
